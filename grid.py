ea0a13c9d240fff9600f15c5b863a2d6a5c0a2b9
amans8270 <amans8270@gmail.com>
2025-11-06 17:10:09 +0530

    grid added , display phase start

---
+++ grid.py
@@ -0,0 +1,25 @@
+class Grid:
+    def __init__(self):
+        self.num_row=20
+        self.num_column=10
+        self.cell_size=30
+        self.grid=[[0 for i in range(self.num_column)]for j in range(self.num_row)]
+        self.colors=self.get_color()
+    def print_grid(self):
+        for i in range(0,self.num_row):
+            for j in range(0,self.num_column):
+                print(self.grid[i][j], end=" ")
+            print()
+    def get_color(self):
+        dark_grey = (26, 31, 40)
+        green = (47, 230, 23)
+        red = (232, 18, 18)
+        orange = (226, 116, 17)
+        yellow = (237, 234, 4)
+        purple = (166, 0, 247)
+        cyan = (21, 204, 209)
+        blue = (13, 64, 216)
+        white = (255, 255, 255)
+        dark_blue = (44, 44, 127)
+        light_blue = (59, 85, 162)
+        return [dark_grey,green,red,orange,yellow,purple,cyan,blue,white,dark_blue,light_blue]
--- sample.py
+++ sample.py
@@ -1,11 +1,13 @@
 import pygame,sys
+from grid import Grid
 pygame.init()
 screen=pygame.display.set_mode((300,600))
 pygame.display.set_caption("Tetris advance version")
 clock=pygame.time.Clock()
 
 dark_blue=(44,44,127)
-
+game_grid=Grid()
+game_grid.print_grid()
 while(True):
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
